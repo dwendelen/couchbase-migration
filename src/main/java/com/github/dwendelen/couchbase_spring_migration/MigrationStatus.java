2e2e210a1b5c90f9c8ee5f571a480be25fa74b30
Daan <daan.wendelen@cegeka.be>
2016-07-15 09:59:29 +0200

    Cleanup

--- src/main/java/com/github/dwendelen/couchbase_spring_migration/MigrationStatus.java
+++ src/main/java/com/github/dwendelen/couchbase_spring_migration/MigrationStatus.java
@@ -1,18 +1,8 @@
 package com.github.dwendelen.couchbase_spring_migration;
 
 public enum MigrationStatus {
-    NOT_STARTED(true),
-    RUNNING(true),
-    FAILED(false),
-    SUCCESS(false);
-
-    private boolean isNotFinished;
-
-    MigrationStatus(boolean isNotFinished) {
-        this.isNotFinished = isNotFinished;
-    }
-
-    public boolean isNotFinished() {
-        return isNotFinished;
-    }
+    NOT_STARTED,
+    RUNNING,
+    FAILED,
+    SUCCESS;
 }
